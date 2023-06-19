18434db6f5a280de6ab1a2fc981e841c1585c585
Oliver Tworkowski <olitworkowski@gmail.com>
2023-06-20 00:14:39 +0200

    declare seeds for attackers and defenders

---
+++ Assets/Code/Scripts/UI/Seed.cs
@@ -0,0 +1,85 @@
+using Game.CustomUI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Game.CustomUI.Seed
+{
+    interface ISeed
+    {
+        public AbilityElement[] GetAbilityElements();
+        public UnitCard[] GetUnitCards();
+        public UpgradeElement[] GetUpgradeElements();
+        public void InflateUI(VisualElement rootVisualElement)
+        {
+            //VisualElement upgradeElementContainer = rootVisualElement.Q<VisualElement>("");
+            VisualElement abilityElementContainer = rootVisualElement.Q<VisualElement>("player-controls__ability-bar");
+            UnitCardPanel unitCardContainer = rootVisualElement.Q<VisualElement>("unit-card-panel") as UnitCardPanel;
+
+            var unitCards = GetUnitCards();
+            foreach (var card in unitCards)
+            {
+                unitCardContainer.AddUnitCard(card);
+            }
+
+            var abilityElements = GetAbilityElements();
+            foreach (var abilityElement in abilityElements)
+            {
+                abilityElement.style.marginBottom = new StyleLength(10);
+                abilityElementContainer.Add(abilityElement);
+            }
+        }
+    }
+
+    #region Seeds
+    public class AttackerSeed : ISeed
+    {
+        public AbilityElement[] GetAbilityElements()
+        {
+            var abilityElements = new List<AbilityElement>();
+            //abilityElements.Add(new AbilityElement());
+            return abilityElements.ToArray();
+        }
+
+        public UnitCard[] GetUnitCards()
+        {
+            var unitCards = new List<UnitCard>();
+            //unitCards.Add(new UnitCard());
+            return unitCards.ToArray();
+        }
+
+        public UpgradeElement[] GetUpgradeElements()
+        {
+            var upgradeElements = new List<UpgradeElement>();
+            //upgradeElements.Add(new UpgradeElement());
+            return upgradeElements.ToArray();
+        }
+    }
+
+    public class DefenderSeed : ISeed
+    {
+        public AbilityElement[] GetAbilityElements()
+        {
+            var abilityElements = new List<AbilityElement>();
+            //abilityElements.Add(new AbilityElement());
+            return abilityElements.ToArray();
+        }
+
+        public UnitCard[] GetUnitCards()
+        {
+            var unitCards = new List<UnitCard>();
+            //unitCards.Add(new UnitCard());
+            return unitCards.ToArray();
+        }
+
+        public UpgradeElement[] GetUpgradeElements()
+        {
+            var upgradeElements = new List<UpgradeElement>();
+            //upgradeElements.Add(new UpgradeElement());
+            return upgradeElements.ToArray();
+        }
+    }
+    #endregion
+}
