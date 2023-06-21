5d0ecdbfd5c0bd835176e92f9fb46fc3fc4e3bd8
Diro Baloska <dirobaloska@outlook.com>
2023-06-21 15:28:18 +0200

    script to build for linux

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -33,4 +33,33 @@ public class Builder
 
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
+
+    [MenuItem("/Build/Linux/Server")]
+    static public void buildLinuxServer() {
+        string[] scenes = {"Assets/Level/Scenes/Netcode/Server.unity", "Assets/Level/Scenes/MainScene.unity"};
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions{
+            scenes = scenes,
+            locationPathName = "Build/Linux/Server/TeamPingu.exe",
+            target = BuildTarget.StandaloneLinux64,
+            subtarget = (int) StandaloneBuildSubtarget.Player,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+
+
+    [MenuItem("Build/Linux/Client")]
+    static public void buildLinuxClient() {
+        string[] scenes = {"Assets/Level/Scenes/Netcode/Client.unity", "Assets/Level/Scenes/MainScene.unity"};
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions{
+            scenes = scenes,
+            locationPathName = "Build/Linux/Client/TeamPingu.exe",
+            target = BuildTarget.StandaloneLinux64,
+            subtarget = (int) StandaloneBuildSubtarget.Player,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
 }
